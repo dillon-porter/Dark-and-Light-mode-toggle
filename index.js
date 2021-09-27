69d4f3f39b32c998ffdb9c8d6c1911e990e45ce3
Dillon Porter <Dillonporter@hotmail.com>
2021-09-26 23:09:10 -0230

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const navBarLinks = document.getElementsByClassName("navbar-links")[0]
+const darkBtn = document.getElementById("dark-btn")
+
+toggleBtn.addEventListener("click", function() {
+    navBarLinks.classList.toggle('active')
+})
+
+darkBtn.addEventListener("click", function(changeText) {
+    document.body.classList.toggle("dark-mode")
+
+    if (changeText.target.textContent === "Dark Mode") {
+        changeText.target.textContent = "Light Mode"
+    } else {
+        changeText.target.textContent = "Dark Mode"
+    }
+
+}) 
